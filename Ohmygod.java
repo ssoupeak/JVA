9f8dbf52aa81a3f24d1292b64fce07574d5429c1
ssoupeak <milleiu0328@naver.com>
2026-04-14 09:36:22 +0900

    Add files via upload

---
+++ Ohmygod.java
@@ -0,0 +1,11 @@
+package IDLE;
+import java.util.HashMap;
+
+public class Ohmygod {
+	public static void main(String[] args) {
+		HashMap<String, String> map = new HashMap<>();
+        map.put("Group", "그룹");
+        map.put("members", "멤버");
+        System.out.println(map.keySet());
+	}
+}
