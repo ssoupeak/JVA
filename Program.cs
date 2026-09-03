dedd1441c751dbb5f8423f37c3d18d76f8bd26e7
ssoupeak <milleiu0328@naver.com>
2026-09-03 11:59:07 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Test
+namespace fdsl
 {
     internal class Program
     {
-        static void Display
-
-
         static void Main(string[] args)
         {
+            int[] scores;
+            scores = new int[5];
+            scores[0] = 1;
+            Console.WriteLine(scores[0]);
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine(scores[i]);
+            }
 
+            float[] data_float;
+            data_float = new float[3] { 1.1f, 2.2f, 3.3f };
+
+            string[] data_str = new string[3] { "aaa", "bbb", "ccc" };
+            for (int i=0; i<data_float.Count(); i++)
+            {
+                Console.WriteLine(data_float[i] + " " + data_str[i]);
+            }
+            foreach (string a in data_str)
+            {
+                Console.WriteLine(a);
+            }
         }
     }
 }
